2e3b81d068f1d770e5c5388bd3a89db2d55802ee
Tibor Both <tibor.both@worldpay.com>
2016-10-11 10:45:06 +0300

    thread call removed

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -22,8 +22,8 @@ public class BaseController {
 		model.addAttribute("message", Counter.TEST);
 		model.addAttribute("counter", counter.getCounter());
 		logger.debug("[welcome] counter : {}", counter.getCounter());
-		Thread myThread = new Thread(new MyThread());
-		myThread.start();
+		//Thread myThread = new Thread(new MyThread());
+		//myThread.start();
 		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return VIEW_INDEX;
 
